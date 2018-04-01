3b3aba39e2b265b3fb1920cdc80d36ef765ccf1a
Harut <harut01harut@gmail.com>
2018-04-01 22:08:07 +0400

    tyjy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,23 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+         int[] i = new int[100];
+
+
+        for (int b=1; b<=100; b++ )
+        {
+
+            if(b%5==0 && b%7==0) {
+                System.out.print("fuzzbuzz");
+            }else if(b%5==0){
+                System.out.print("fuzz");
+            }else if(b%7==0){
+                System.out.print("buzz");
+            }else{
+                System.out.print(b);
+            }
+            System.out.print(" ");
+        }
 
 
     }
